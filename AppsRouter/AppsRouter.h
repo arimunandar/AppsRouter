c69c8d15e1a7a51a3f3ca5c1c86dc4146ffbc5b4
Ari Munandar <ari@stockbit.com>
2022-01-02 16:49:14 +0700

    Initial project setup

---
+++ AppsRouter/AppsRouter.h
@@ -0,0 +1,18 @@
+//
+//  AppsRouter.h
+//  AppsRouter
+//
+//  Created by Ari Munandar on 02/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppsRouter.
+FOUNDATION_EXPORT double AppsRouterVersionNumber;
+
+//! Project version string for AppsRouter.
+FOUNDATION_EXPORT const unsigned char AppsRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppsRouter/PublicHeader.h>
+
+
---
+++ AppsRouterExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppsRouterExamples
+//
+//  Created by Ari Munandar on 02/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
